f1a0b9ff3f851f355aa3b85b84d4429b617dd1eb
Michael Strobel <mstro016@ucr.edu>
2024-07-01 11:25:41 -0700

    Working parallel inference, basic metrics.

---
+++ src/ms2deepscore/test_presampled.py
@@ -0,0 +1,407 @@
+from pathlib import Path
+from glob import glob
+import os
+import gc
+import sys
+import argparse
+import pickle
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+from matchms.importing import load_from_mgf
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.callbacks import (EarlyStopping, ModelCheckpoint)
+from tensorflow.keras.optimizers import Adam
+import pandas as pd
+import numpy as np
+
+from ms2deepscore import SpectrumBinner
+from ms2deepscore.data_generators import DataGeneratorAllInchikeys
+from ms2deepscore.models import SiameseModel
+from ms2deepscore import MS2DeepScore
+from ms2deepscore.models import load_model
+
+from ms2deepscore.vector_operations import cosine_similarity
+
+from tqdm import tqdm
+from joblib import Parallel, delayed
+import tempfile
+
+import dask.dataframe as dd
+from time import time
+
+from dask.diagnostics import ProgressBar
+ProgressBar().register()
+
+sys.path.append('../shared')
+from utils import train_test_similarity_dependent_losses, \
+                    tanimoto_dependent_losses, \
+                    train_test_similarity_heatmap, \
+                    train_test_similarity_bar_plot, \
+                    fixed_tanimoto_train_test_similarity_dependent
+                    
+plt.rcParams.update({
+    "text.usetex": False,
+    "font.size": 20,
+    "axes.titlesize": 20,
+    "axes.labelsize": 20,
+    "xtick.labelsize": 18, #
+    "ytick.labelsize": 18, #
+    "legend.fontsize": 18, #
+    "figure.titlesize": 20,
+    "legend.title_fontsize": 20, #
+    "figure.autolayout": True,
+    "figure.dpi": 300,
+    })
+
+# def _parallel_cosine(dask_partition,
+#                      embedding_dict,
+#                      buffer_size=7_900_000): 
+#     # Buffer Size Calculation: 
+#     # 80 gb (leaving 40 for overhead) / 32 / 254 bytes per row * 0.75 for extra safety
+
+
+#     hdf_store = None
+#     hdf_path = None
+#     try:
+#         # Tempfile for hdf storage
+#         hdf_path = tempfile.NamedTemporaryFile(delete=False)
+
+#         # Calculate the similarity list in a memory-efficent manner
+#         hdf_store = pd.HDFStore(hdf_path.name)
+        
+#         max_main_train_test_sim  = train_test_sim_dict[main_inchikey]['max']
+#         min_main_train_test_sim  = train_test_sim_dict[main_inchikey]['min']
+#         mean_main_train_test_sim = train_test_sim_dict[main_inchikey]['mean']
+
+#         columns=['inchikey1', 'inchikey2','spectrumid1', 'spectrumid2', 'ground_truth_similarity', 'mean_mean_train_test_sim',
+#                 'max_max_train_test_sim', 'max_mean_train_test_sim', 'max_min_train_test_sim', 'pred', 'error'] # Only error and pred are new here
+
+
+#         output_list = []
+#         curr_buffer_size = buffer_size
+
+#         def _helper(spectrumid1, spectrumid2):
+#             return cosine_similarity(embedding_dict[spectrumid1]['embedding'], embedding_dict[spectrumid2]['embedding'])
+        
+#         spectrumid1_lst = dask_partition['spectrum_id1'].values
+#         spectrumid2_lst = dask_partition['spectrum_id2'].values
+
+        
+
+#         # Dump the remaining content
+#         start_time = time()
+#         output_frame = pd.DataFrame(output_list, columns=columns)
+#         # print(f"Creating dataframe took {time() - start_time} seconds")
+#         start_time = time()
+#         hdf_store.put(f"{main_inchikey}", output_frame, format='table', append=True, track_times=False,
+#                         min_itemsize={'spectrumid1': 50, 'spectrumid2': 50})
+#         # print(f"Storing took {time() - start_time} seconds")
+#         curr_buffer_size = buffer_size
+#         output_list = []
+
+#         hdf_store.close()
+#         hdf_path.close()
+#     except Exception as e:
+#         # Close the hdf store if needed and exists
+#         if hdf_store is not None and hdf_store.is_open:
+#             hdf_store.close()
+
+#         # Delete the hdf file if needed
+#         if os.path.exists(hdf_path.name):
+#             os.remove(hdf_path.name)
+
+#         raise e
+#     except KeyboardInterrupt as ki:
+#         # Close the hdf store if needed and exists
+#         if hdf_store is not None and hdf_store.is_open:
+#             hdf_store.close()
+
+#         # Delete the hdf file if needed
+#         if os.path.exists(hdf_path.name):
+#             os.remove(hdf_path.name)
+
+#         raise ki
+
+#     return hdf_path.name # Cleanup in serial process after concat
+
+def _dask_cosine(df, embedding_dict):
+    def _helper(row):
+        if row.spectrumid1 not in embedding_dict or row.spectrumid2 not in embedding_dict:
+            return -1.0
+        return cosine_similarity(embedding_dict[row.spectrumid1], embedding_dict[row.spectrumid2])
+    out = df.apply(_helper, axis=1)
+    # For some reason out is a dataframe with a single column, rather than a series
+    out = out.rename('predicted_similarity')
+    if isinstance(out, pd.DataFrame):
+        return pd.Series(out.predicted_similarity)
+    else:
+        return pd.Series(out)
+
+def main():
+    parser = argparse.ArgumentParser(description='Test MS2DeepScore on the original data')
+    parser.add_argument('--test_path', type=str, help='Path to the test data')      # Path to pickle file of spectra
+    parser.add_argument('--presampled_pairs_path', type=str, help='Path to dask parquet file of presampled pairs')
+    parser.add_argument("--save_dir", type=str, help="Path to the model")
+    parser.add_argument("--model_path", type=str, help="Path to the model, overrides n_most_recent", default=None)
+    parser.add_argument("--save_dir_insert", type=str, help="Appended to save dir, to help organize test sets", default="")
+    parser.add_argument("--n_most_recent", type=int, help="Number of most recent models to evaluate", default=None)
+    parser.add_argument("--n_jobs", type=int, default=1, help="Number of jobs to run in parallel")
+    args = parser.parse_args()
+    
+    
+    # Check if GPU is available
+    print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE", flush=True)
+    
+    n_most_recent = args.n_most_recent
+    # List available models
+    print("Available models:")
+    if args.n_most_recent is None:
+        available_models = [Path(args.model_path)]
+        assert os.path.isfile(available_models[0]), f"Model path {available_models[0]} is not a file."
+        n_most_recent = 1
+    else:
+        available_models = [model for model in Path(args.save_dir).rglob("*.hdf5")][:n_most_recent]
+    print(available_models)
+    
+    print(f"The most recent {n_most_recent} models will be evaluated.")
+    
+    # Sort models by datetime:  dd_mm_yyyy_hh_mm_ss format "%d_%m_%Y_%H_%M_%S"
+    available_models = sorted(available_models, key=lambda x: datetime.strptime(x.stem.split('model_')[1], "%d_%m_%Y_%H_%M_%S"), reverse=True)
+    
+    
+    print("Testing the following models:", available_models[:n_most_recent], flush=True)
+    for model_index, model_name in enumerate(available_models[:n_most_recent]):
+        print(f"Loading model ({model_index+1}/{min(len(available_models), n_most_recent)}: {model_name.stem})...", flush=True)
+        model = load_model(model_name)
+        print("\tDone.", flush=True)
+
+        spectra_test = pickle.load(open(args.test_path, "rb"))
+
+        print("Loading Presampled Pairs", flush=True)
+        presampled_pairs_path = Path(args.presampled_pairs_path)
+        presampled_pairs = dd.read_parquet(presampled_pairs_path)
+        print("\tDone.", flush=True)
+                
+        metric_dir = os.path.join(args.save_dir, model_name.stem, presampled_pairs_path.stem, args.save_dir_insert)
+            
+        print("Saving Metrics to:", metric_dir)
+        if not os.path.isdir(metric_dir):
+            os.makedirs(metric_dir, exist_ok=True)
+        print("\tDone.", flush=True)
+               
+        all_test_inchikeys = [s.get("inchikey")[:14] for s in spectra_test]
+        test_inchikeys = np.unique(all_test_inchikeys)
+        print(f"Got {len(test_inchikeys)} inchikeys in the test set.")
+        
+        print("Performing Inference...", flush=True)
+        similarity_score = MS2DeepScore(model,)
+        df_labels = [s.get("spectrum_id") for s in spectra_test]
+
+        # Use MS2DeepScore to embed the vectors
+        embedded_spectra = similarity_score.calculate_vectors([spectra_test[0]])        # DEBUG USING ONE EMBEDDING
+        embedded_spectra = [embedded_spectra[0] for _ in range(len(spectra_test))]
+
+        # Create a dictionary of spectrum_ids to embeddings
+        spectrumid_to_embedding_dict = {s.get("title"): e for s, e in zip(spectra_test, embedded_spectra)}
+        
+        # Get first two partitons #DEBUG
+        presampled_pairs = presampled_pairs.partitions[:3]
+
+        # Compute parallel scores for all pairs
+        meta = _dask_cosine(presampled_pairs.head(2),
+                            embedding_dict=spectrumid_to_embedding_dict)
+        presampled_pairs['predicted_similarity'] = presampled_pairs.map_partitions(_dask_cosine,
+                                                            embedding_dict=spectrumid_to_embedding_dict,
+                                                            meta=meta   # Must provide metadata to avoid fake inputs used for type prediction
+                                                            )
+        predictions = presampled_pairs.compute()
+
+        print(presampled_pairs.head(50))
+
+        # Replace -1 with nan for predictions
+        presampled_pairs.predicted_similarity = presampled_pairs.predicted_similarity.replace(-1, np.nan)
+       
+        # Add the ground_true value to the predictions
+        print("Calculating Error")
+        presampled_pairs['error'] = (presampled_pairs['predicted_similarity'] - presampled_pairs['ground_truth_similarity']).abs()
+
+        
+        ref_score_bins = np.linspace(0.2,1.0, 17)   # TODO: CHANGE THIS?
+        
+        overall_rmse = np.sqrt(np.mean(np.square(presampled_pairs['error'].values.compute())))
+        print("Overall RMSE (from evaluate()):", overall_rmse)
+        overall_mae =  np.mean(np.abs(presampled_pairs['error'].values.compute()))
+        print("Overall MAE (from evaluate()):", overall_mae)
+
+        # Nan Safe
+        print("Overall NAN RMSE (from evaluate()):", np.sqrt(np.nanmean(np.square(presampled_pairs['error'].values))).compute())
+        print("Overall NAN MAE (from evaluate()):", np.nanmean(np.abs(presampled_pairs['error'].abs().nanmean)).compute())
+
+        # DEBUG
+        sys.exit(0)
+
+        # Train-Test Similarity Dependent Losses
+        similarity_dependent_metrics_mean = train_test_similarity_dependent_losses(predictions, train_test_similarities, ref_score_bins, mode='mean')
+        similarity_dependent_metrics_max = train_test_similarity_dependent_losses(predictions, train_test_similarities, ref_score_bins, mode='max')
+        train_test_grid = train_test_similarity_heatmap(predictions, train_test_similarities, ref_score_bins)
+        # Returns {'bin_content':bin_content_grid, 'bounds':bound_grid, 'rmses':rmse_grid, 'maes':mae_grid}
+        
+        grid_fig_size = (10,10)
+        plt.figure(figsize=grid_fig_size)
+        plt.title(f'Train-Test Dependent RMSE\nAverage RMSE: {overall_rmse:.2f}')
+        plt.imshow(train_test_grid['rmses'], vmin=0)
+        plt.colorbar()
+        
+        tick_labels = [f'({x[1][0]:.2f}, {x[1][1]:.2f})' for x in train_test_grid['bounds'][0]]
+        
+        plt.xticks(ticks=np.arange(0,len(train_test_grid['rmses'])), labels=tick_labels, rotation=90)
+        plt.yticks(ticks=np.arange(0,len(train_test_grid['rmses'])), labels=tick_labels,)
+        plt.xlabel("Max Test-Train Stuctural Similarity")
+        plt.ylabel("Max Test-Train Stuctural Similarity")
+        plt.savefig(os.path.join(metric_dir, 'heatmap.png'))
+        # Train-Test Similarity Dependent Counts
+        plt.figure(figsize=grid_fig_size)
+        plt.title('Train-Test Dependent Counts')
+        plt.imshow(train_test_grid['bin_content'], vmin=0)
+        plt.colorbar()
+    
+        plt.xticks(ticks=np.arange(0,len(train_test_grid['rmses'])), labels=tick_labels, rotation=90)
+        plt.yticks(ticks=np.arange(0,len(train_test_grid['rmses'])), labels=tick_labels,)
+        plt.xlabel("Max Test-Train Stuctural Similarity")
+        plt.ylabel("Max Test-Train Stuctural Similarity")
+        # If the number of samples in a bin is less than 30, put text in the bin
+        for i in range(len(train_test_grid['rmses'])):
+            for j in range(len(train_test_grid['rmses'])):
+                if train_test_grid['bin_content'][i,j] < 30 and not pd.isna(train_test_grid['bin_content'][i,j]):
+                    plt.text(j, i, f'{train_test_grid["bin_content"][i,j]:.0f}', ha="center", va="center", color="white")
+
+        plt.savefig(os.path.join(metric_dir, 'heatmap_counts.png'))
+        # Train-Test Similarity Dependent Nan-Counts
+        plt.figure(figsize=grid_fig_size)
+        plt.title('Train-Test Dependent Nan-Counts')
+        plt.imshow(train_test_grid['nan_count'], vmin=0)
+        plt.colorbar()
+        plt.xticks(ticks=np.arange(0,len(train_test_grid['rmses'])), labels=tick_labels, rotation=90)
+        plt.yticks(ticks=np.arange(0,len(train_test_grid['rmses'])), labels=tick_labels,)
+        plt.savefig(os.path.join(metric_dir, 'heatmap_nan_counts.png'))
+        
+            
+        # Train-Test Similarity Dependent Losses Aggregated
+        plt.figure(figsize=(12, 9))
+        plt.bar(np.arange(len(similarity_dependent_metrics_max["rmses"]),), similarity_dependent_metrics_max["rmses"],)
+        plt.title(f'Train-Test Dependent RMSE\nAverage RMSE: {overall_rmse:.2f}')
+        plt.xlabel("Max Test-Train Stuctural Similarity")
+        plt.ylabel("Max Test-Train Stuctural Similarity")
+        plt.ylabel("RMSE")
+        plt.xlabel("Tanimoto score bin")
+        plt.xticks(np.arange(len(similarity_dependent_metrics_max["rmses"])), [f"{a:.2f} to < {b:.2f}" for (a, b) in similarity_dependent_metrics_max["bounds"]], rotation='vertical')
+        plt.grid(True)
+        plt.savefig(os.path.join(metric_dir, 'train_test_rmse.png'))
+        
+        
+        # MS2DeepScore Tanimoto Dependent Losses Plot
+        ref_score_bins = np.linspace(0,1.0, 11)
+
+        tanimoto_dependent_dict = tanimoto_dependent_losses(predictions, ref_score_bins)
+
+        
+        metric_dict = {}
+        metric_dict["bin_content"]      = tanimoto_dependent_dict["bin_content"]
+        metric_dict["nan_bin_content"]  = tanimoto_dependent_dict["nan_bin_content"]
+        metric_dict["bounds"]           = tanimoto_dependent_dict["bounds"]
+        metric_dict["rmses"]            = tanimoto_dependent_dict["rmses"]
+        metric_dict["maes"]             = tanimoto_dependent_dict["maes"]
+        metric_dict["rmse"] = overall_rmse
+        metric_dict["mae"]  = overall_mae
+        
+        # Fixed-Tanimoto, Train-Test Dependent Plot
+        ref_score_bins = np.linspace(0,1.0, 11)
+        ftttsd = fixed_tanimoto_train_test_similarity_dependent(predictions, train_test_similarities, ref_score_bins)
+        
+        # Save to pickle
+        metric_path = os.path.join(metric_dir, "metrics.pkl")
+        print(f"Saving metrics to {metric_path}")
+        fixed_tanimoto_train_test_similarity_dependent_path = os.path.join(metric_dir, "fixed_tanimoto_train_test_similarity_dependent_path.pkl")
+        pickle.dump(ftttsd, open(fixed_tanimoto_train_test_similarity_dependent_path, "wb"))
+        pickle.dump(metric_dict, open(metric_path, "wb"))
+        train_test_metric_path = os.path.join(metric_dir, "train_test_metrics_mean.pkl")
+        pickle.dump(similarity_dependent_metrics_mean, open(train_test_metric_path, "wb"))
+        train_test_metric_path = os.path.join(metric_dir, "train_test_metrics_max.pkl")
+        pickle.dump(similarity_dependent_metrics_max, open(train_test_metric_path, "wb"))
+        train_test_metric_path = os.path.join(metric_dir, "train_test_grid.pkl")
+        pickle.dump(train_test_grid, open(train_test_metric_path, "wb"))
+
+        fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(6, 8))
+        
+        ax1.plot(np.arange(len(metric_dict["rmses"])), metric_dict["rmses"], "o:", color="crimson")
+        ax1.set_title('RMSE')
+        ax1.set_ylabel("RMSE")
+        ax1.grid(True)
+
+        ax2.plot(np.arange(len(metric_dict["rmses"])), metric_dict["bin_content"], "o:", color="teal")
+        ax2.plot(np.arange(len(metric_dict["rmses"])), metric_dict["nan_bin_content"], "o:", color="grey")
+        if sum(metric_dict["nan_bin_content"]) > 0:
+            ax2.legend(["# of valid spectrum pairs", "# of nan spectrum pairs"])
+        ax2.set_title('# of spectrum pairs')
+        ax2.set_ylabel("# of spectrum pairs")
+        ax2.set_xlabel("Tanimoto score bin")
+        plt.yscale('log')
+        plt.xticks(np.arange(len(metric_dict["rmses"])), [f"{a:.1f} to < {b:.1f}" for (a, b) in metric_dict["bounds"]], rotation='vertical')
+        ax2.grid(True)
+        
+        # Save figure
+        fig_path = os.path.join(metric_dir, "metrics.png")
+        plt.savefig(fig_path)
+        
+        # spec2vec_percentile_plot(predictions, scores_ref, metric_dir)
+        
+        # Train-Test Similarity Bar Plot
+        train_test_sim = train_test_similarity_bar_plot(predictions, train_test_similarities, ref_score_bins)
+        bin_content, bounds = train_test_sim['bin_content'], train_test_sim['bounds']
+        plt.figure(figsize=(12, 9))
+        plt.title("Number of Structures in Similarity Bins (Max Similarity to Train Set)")
+        plt.bar(range(len(bin_content)), bin_content, label='Number of Structures')
+        plt.xlabel('Similarity Bin (Max Similarity to Train Set)')
+        plt.ylabel('Number of Structures')
+        plt.xticks(range(len(bin_content)), [f"({bounds[i][0]:.2f}-{bounds[i][1]:.2f})" for i in range(len(bounds))], rotation=45)
+        plt.legend()
+        plt.savefig(os.path.join(metric_dir, 'train_test_similarity_bar_plot.png'), bbox_inches="tight")
+        
+        # Score, Tanimoto Scatter Plot
+        plt.figure(figsize=grid_fig_size)
+        plt.scatter(predictions['score'], predictions['tanimoto'], alpha=0.2)
+        # Show R Squared
+        r2 = np.corrcoef(predictions['score'], predictions['tanimoto'])[0,1]**2
+        # Plot y=x line
+        plt.plot([0, 1], [0, 1], color='red', linestyle='--')
+        plt.xlabel('Predicted Spectral Similarity Score')
+        plt.ylabel('Tanimoto Score')
+        # Make square
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.title(f'Predicted vs Reference Spectral Similarity Scores \n(R squared = {r2:.2f})')
+        plt.savefig(os.path.join(metric_dir, 'predicted_vs_reference.png'))
+        
+        # Score, Tanimoto Binned Histograms
+        # This should show 10 histograms vertically stacked that correspond to 10 different tanimoto similarity bins
+        #plt.figure(figsize=(20,20))
+        # TODO    
+        
+        # Score, Tanimoto Scatter Plot (Hexbin)
+        plt.figure(figsize=grid_fig_size)    
+        hb = plt.hexbin(predictions['score'], predictions['tanimoto'], gridsize=50, cmap='inferno', bins='log')
+        cb = plt.colorbar(hb)
+        cb.set_label('log counts')
+        plt.xlabel('Predicted Spectral Similarity Score')
+        plt.ylabel('Tanimoto Score')
+        # Make square
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.title('Predicted vs Reference Spectral Similarity Scores')
+        plt.savefig(os.path.join(metric_dir, 'predicted_vs_reference_hexbin.png'))
+
+    
+if __name__ == "__main__":
+    main()
