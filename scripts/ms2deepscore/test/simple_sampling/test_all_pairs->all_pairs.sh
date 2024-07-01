4b323f50c320e5083abea0317d41303dcc6823e5
Michael Strobel <mstro016@ucr.edu>
2024-07-01 16:00:02 -0700

    Update test script.

--- scripts/ms2deepscore/test/simple_sampling/test_all_pairs->all_pairs.sh
+++ scripts/ms2deepscore/test/simple_sampling/test_all_pairs->all_pairs.sh
@@ -3,7 +3,7 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 
 BIN_DIR="../../../../src/ms2deepscore/"
 
-DATA_DIR="/data/nas-gpu/SourceCode/michael_s/Baselines_For_Benchmark/data/structural_similarity_basic/processed"
+DATA_DIR="../../../../data/structural_similarity_basic/processed"
 MODEL_PATH="./simple_sampling/model_21_06_2024_09_13_17.hdf5"
 
 conda activate "$BIN_DIR/../shared/conda_env"
@@ -13,7 +13,7 @@ echo "Performing Inference for MS2DeepScore with a basic split, filtered by pair
 cd $BIN_DIR
 
 python3 test.py --test_path $DATA_DIR"/data/ALL_GNPS_positive_test_split.pickle" \
-                 --tanimoto_path $DATA_DIR"/test_tanimoto_df.csv" \
-                 --train_test_similarities $DATA_DIR"/train_test_tanimoto_df.csv" \
+                 --presampled_pairs_path $DATA_DIR"/test_data/all_pairs.parquet" \
                  --save_dir "./simple_sampling" \
+                 --n_jobs 30 \
                  --model_path $MODEL_PATH
