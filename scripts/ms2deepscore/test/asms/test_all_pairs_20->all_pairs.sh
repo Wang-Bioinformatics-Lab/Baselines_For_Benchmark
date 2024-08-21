64d2d6e1e99df241d1df89a4c3c5a39bedb34195
Michael Strobel <mstro016@ucr.edu>
2024-08-21 09:13:00 -0700

    Incrimentally Upload Files.

---
+++ scripts/ms2deepscore/test/asms/test_all_pairs_20->all_pairs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+
+BIN_DIR="../../../../src/ms2deepscore/"
+
+DATA_DIR="../../data/asms/processed"
+MODEL_PATH="./asms/model_15_07_2024_16_03_15.hdf5"
+
+conda activate "$BIN_DIR/../shared/new_conda_env"
+
+echo "Performing Inference for MS2DeepScore with a basic split, filtered by pairs"
+
+cd $BIN_DIR
+
+python3 test_presampled.py --test_path $DATA_DIR"/data/ALL_GNPS_positive_test_split.pickle" \
+                            --presampled_pairs_path $DATA_DIR"/test_data/all_pairs_new.parquet" \
+                            --save_dir "./asms" \
+                            --n_jobs 30 \
+                            --model_path $MODEL_PATH
